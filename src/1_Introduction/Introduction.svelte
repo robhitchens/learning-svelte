271433b9da112efac072a919df53f44591530d1c
hitc6176 <rhitchens@wsfsbank.com>
2020-01-17 15:19:48 -0500

    still going through bindings

--- src/1_Introduction/Introduction.svelte
+++ src/1_Introduction/Introduction.svelte
@@ -19,6 +19,15 @@
     import ComponentEvents from "../5_Events/ComponentEvents.svelte";
     import EventForwarding from "../5_Events/EventForwarding.svelte";
     import DomEventForwarding from "../5_Events/DomEventForwarding.svelte";
+    import TextInputs from "../6_Bindings/TextInputs.svelte";
+    import NumericInputs from "../6_Bindings/NumericInputs.svelte";
+    import CheckboxInputs from "../6_Bindings/CheckboxInputs.svelte";
+    import GroupInputs from "../6_Bindings/GroupInputs.svelte";
+    import TextareaInputs from "../6_Bindings/TextareaInputs.svelte";
+    import SelectBindings from "../6_Bindings/SelectBindings.svelte";
+    import SelectMultiple from "../6_Bindings/SelectMultiple.svelte";
+    import ContentEditableBindings from "../6_Bindings/ContentEditableBindings.svelte";
+    import EachBlockBindings from "../6_Bindings/EachBlockBindings.svelte";
 
     let name = 'world';
 </script>
@@ -44,3 +53,12 @@
 <ComponentEvents/>
 <EventForwarding/>
 <DomEventForwarding/>
+<TextInputs/>
+<NumericInputs/>
+<CheckboxInputs/>
+<GroupInputs/>
+<TextareaInputs/>
+<SelectBindings/>
+<SelectMultiple/>
+<ContentEditableBindings/>
+<EachBlockBindings/>
